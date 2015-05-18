68686b38fbd702ee93ac26cfcb76a06d8d2b0206
30119267 <30119267@mth-f305a-pc08.students.uob.ballarat.edu.au>
2015-05-18 17:25:23 +1000

    S1 test

--- OpenNic DNS Server Switch/src/run/Program.java
+++ OpenNic DNS Server Switch/src/run/Program.java
@@ -1,17 +1,38 @@
 package run;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 
 public class Program {
 
-	Option help = new Option("help", "print this message");
-	Option s1 = Option.builder("s1")
-			.required(true)
+	static Option help = new Option("help", "print this message");
+	static Option s1 = Option.builder("s1")
 			.longOpt("Primary Domain Server")
 			.build();
-	Option s2 = new Option("s2", true, "Second Domain Server");
+	static Option s2 = new Option("s2", true, "Second Domain Server");
+	static Options options = new Options();
 	
 	public static void main(String[] args) {
+		options.addOption(s1);
+		options.addOption(s2);
+		String testing = "S1 not set";
+		CommandLineParser parser = new DefaultParser();
+		try {
+			CommandLine line = parser.parse(options,  args);
+			if (line.hasOption("s1")) {
+				testing = "S1 has been set";
+			}
+			System.out.print(testing);
+			
+			
+		}
+		catch (ParseException exp) {
+			System.err.println("Parsing failed. Reason: " + exp.getMessage());
+		}
 		
 	}
 }
