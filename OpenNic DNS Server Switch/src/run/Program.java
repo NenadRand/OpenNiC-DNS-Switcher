c9c63518926bc8bce107255eeba35f2436abb941
30119267 <30119267@mth-f305a-pc08.students.uob.ballarat.edu.au>
2015-05-18 17:06:00 +1000

    Testing apache cli

--- OpenNic DNS Server Switch/src/run/Program.java
+++ OpenNic DNS Server Switch/src/run/Program.java
@@ -1,7 +1,16 @@
 package run;
+import org.apache.commons.cli.Option;
+
 
 public class Program {
 
+	Option help = new Option("help", "print this message");
+	Option s1 = Option.builder("s1")
+			.required(true)
+			.longOpt("Primary Domain Server")
+			.build();
+	Option s2 = new Option("s2", true, "Second Domain Server");
+	
 	public static void main(String[] args) {
 		
 	}
